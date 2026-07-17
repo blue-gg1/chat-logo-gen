c29cd73d379e650089131e06b406d3150f3f69c9
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-17 11:16:29 +0300

    thats wrong?

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def TestTextToIcon(Text, TemplateImage, Font, FilePath):
     draw.text(
         (100,250), # text space
         Text, # the words themeseves 
-        (181, 32, 37), # colour in rbg
+        (255, 255, 255), # colour in rbg
         font=monospace) # which font
 
     
