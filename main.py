fe7742eee97b25fe01e844cb07f9e30cf316edc1
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-17 11:09:35 +0300

    made the rgb look like the one here (looks bad) https://openscholar.huji.ac.il/branden/book/graphics-typography-shapes-and-colors

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def TestTextToIcon(Text, TemplateImage, Font, FilePath):
     #     print(Row)
     #     LoopCount += LineOffset
 
-    draw.text((0,0),Text,(255,255,255),font=monospace,align='center')
+    draw.text((0,0),Text,(70, 26, 64),font=monospace,align='center')
 
     
     
