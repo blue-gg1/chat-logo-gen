cfc894775fa0cc7e9e4cafb9ea83d882b8da02ad
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-17 11:08:55 +0300

    its working now

--- main.py
+++ main.py
@@ -39,8 +39,47 @@ def TextToIcon(Chunks, Text, TemplateImage, Font, FilePath):
     result_o = np.array(im_p)
     cv2.imwrite(FilePath, result_o)
 
-TextToIcon(10, """test
+# TextToIcon(10, """test
+# TEST""", "Template.png","fonts/3270SemiCondensed-Regular.ttf","pfp.jpg")
+
+
+
+
+
+def TestTextToIcon(Text, TemplateImage, Font, FilePath):
+    print(Text)
+
+    # Open image with OpenCV
+    # im_o = np.zeros((1000,1000,3), np.uint8)
+    im_o = cv2.imread(TemplateImage)
+
+
+    # Make into PIL Image
+    im_p = Image.fromarray(im_o)
+
+    # Get a drawing context
+    draw = ImageDraw.Draw(im_p)
+    monospace = ImageFont.truetype(Font,200)
+
+    TopOffset = 300
+    SideOffSet = 150
+    LineOffset = 0
+
+    
+    # LoopCount = TopOffset
+    # for Chunk in TextChunks:
+    #     Row = (SideOffSet, LoopCount)
+    #     draw.text((Row),Chunk,(255,255,255),font=monospace,align='center')
+    #     print(Row)
+    #     LoopCount += LineOffset
+
+    draw.text((0,0),Text,(255,255,255),font=monospace,align='center')
+
+    
+    
+    # #Save image
+    result_o = np.array(im_p)
+    cv2.imwrite(FilePath, result_o)
+
+TestTextToIcon("""test
 TEST""", "Template.png","fonts/3270SemiCondensed-Regular.ttf","pfp.jpg")
-
-
-
