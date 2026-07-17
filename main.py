6f29fa90b9800fa6f0a532a536778c5c9749421b
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-17 10:41:20 +0300

    the code works i guess

--- main.py
+++ main.py
@@ -4,31 +4,6 @@ import cv2
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
-
-NATO_LIST = []
-NATO_FILES_LIST = []
-BUMPER_FILE_LIST = []
-
-
-def SET_VARS():
-    global now_filename
-    global NAME_LETTER_MP3
-    global NAME_MUSAK_MP3
-    global NAME_ICON_PNG
-    global now
-    global this_hour
-    now = datetime.now(timezone.utc)
-    today = date.today()
-    this_year = str(today.year)
-    this_month = str(today.month)
-    this_day = str(today.day)
-    this_hour = str(int(now.strftime("%H"))+1)
-    now_filename = this_year+"-"+this_month+"-"+this_day+"-"+this_hour
-    NAME_LETTER_MP3 = now_filename+"-LETTERS-TEST.mp3"
-    NAME_MUSAK_MP3 = now_filename+"-MUSAK-TEST.mp3"
-    NAME_ICON_PNG = now_filename+"-ICON-TEST.png"
-
-
 def generate_random_alphanumeric(length):
     characters = string.ascii_uppercase
     random_string = ''.join(random.choices(characters, k=length))
@@ -36,8 +11,8 @@ def generate_random_alphanumeric(length):
 
 
 def TEXT_TO_ICON(Chunks, Text, FilePath):
-    NATO_chunks = textwrap.wrap(Text, Chunks)
-    print(NATO_chunks)
+    TextChunks = textwrap.wrap(Text, Chunks)
+    print(TextChunks)
 
     # Open image with OpenCV
     im_o = np.zeros((1000,1000,3), np.uint8)
@@ -55,7 +30,7 @@ def TEXT_TO_ICON(Chunks, Text, FilePath):
 
 
     LoopCount = TopOffset
-    for Chunk in NATO_chunks:
+    for Chunk in TextChunks:
         Row = (SideOffSet, LoopCount)
         draw.text((Row),Chunk,(255,255,255),font=monospace,align='right')
         print(Row)
@@ -65,10 +40,7 @@ def TEXT_TO_ICON(Chunks, Text, FilePath):
     result_o = np.array(im_p)
     cv2.imwrite(FilePath, result_o)
 
-
-# SET_VARS()
-# NATO_MP3(128)
-TEXT_TO_ICON(11, "LOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUM", "pfp.jpg")
+TEXT_TO_ICON(11, "AnotherTESTLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUMLOREMIPSUM", "pfp.jpg")
 
 
 
