b62c7823ae5cd488421c03f82773dfe25d9fd6d3
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-26 17:19:04 +0300

    more space

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def TextToIcon(Text, TemplateImage, Font, FilePath):
     monospace = ImageFont.truetype(Font,125)
 
     draw.multiline_text(
-        (im_p.width / 2, (im_p.height / 2)+10),
+        (im_p.width / 2, (im_p.height / 2)+30),
         Text,
         fill="white",
         font=monospace,
