e2ef860d64dfa78c4271f2ffbad07e00f7c1a3f1
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-26 22:07:08 +0300

    english issues

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def AddTextToImageAndDealWithString(Text, OutputFile, Year, CouseNumber):
         # StringForImage += (line[::-1])+"\n"
         StringForImage += get_display(line)+"\n" # revese the string in a way that does not change any latin text
         
-    TextToIcon(StringForImage+Year, "Template.png", "fonts/FRANKB.TTF", OutputFile, CouseNumber)
+    TextToIcon(StringForImage+Year, "Template.png", "fonts/FRANK-B.TTF", OutputFile, CouseNumber)
 
 def GetCourseFromFile():
     # read the file from disk.
