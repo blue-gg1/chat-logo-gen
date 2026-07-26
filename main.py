d3c43933a9a06288611f6ce44bac02bb9215a1a1
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-26 18:00:14 +0300

    what if it was bright?

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def TextToIcon(Text, TemplateImage, Font, FilePath, CourseSeed):
 
     draw.rectangle(
         (0,0,1000,1000),
-        fill = (randint(0,255),randint(0,255),randint(0,255))
+        fill = (randint(200,255),randint(200,255),randint(200,255))
     )
 
     draw.ellipse(
