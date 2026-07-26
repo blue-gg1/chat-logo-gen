95c54c2020073f88a038b4a62fdf862b0156eb95
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-07-26 21:47:12 +0300

    added comments and renamed functions.

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def TextToIcon(Text, TemplateImage, Font, FilePath, CourseSeed):
     # Open image with OpenCV
     im_o = np.zeros((1000,1000,3), np.uint8)
 
-    # im_o = cv2.imread(TemplateImage)
-
+    # im_o = cv2.imread(TemplateImage) # read from image.
 
     # Make into PIL Image
     im_p = Image.fromarray(im_o)
@@ -19,29 +18,30 @@ def TextToIcon(Text, TemplateImage, Font, FilePath, CourseSeed):
     draw = ImageDraw.Draw(im_p)
     monospace = ImageFont.truetype(Font,125)
 
-    draw.rectangle(
+    draw.rectangle( # make the backaround a solid colour
         (0,0,1000,1000),
-        fill = (randint(0,50),randint(0,50),randint(0,50))
+        fill = (randint(0,50),randint(0,50),randint(0,50)) # make it a dark pallet 
     )
 
-    draw.ellipse(
-        (20, 20, 1000, 1000), 
-        fill = (randint(0,255),randint(0,255),randint(0,255)), 
-        outline = (randint(0,255),randint(0,255),randint(0,255)),
+    draw.ellipse( # draw the main circle 
+        (20, 20, 1000, 1000), # circle covers the entire image 
+        fill = (randint(0,255),randint(0,255),randint(0,255)),  # random colour for the circle
+        outline = (randint(0,255),randint(0,255),randint(0,255)), # random colour for the outline 
         width=20
     )
 
-    draw.multiline_text(
+    draw.multiline_text( # place the text
         # (im_p.width / 2, (im_p.height / 2)+40),
-        (im_p.width / 2, (im_p.height / 2)),
+        (im_p.width / 2, (im_p.height / 2)), # make it be the middle 
         Text,
-        fill="black",
+        fill="black", # make the text black
         font=monospace,
         align="center",
-        anchor="mm",
-        stroke_width=7,
-        stroke_fill="white"
+        anchor="mm", # make it the middle
+        stroke_width=7, # add the outline
+        stroke_fill="white" # make the outline white
     )
+    # old manual placement of the text
     # draw.text(
     #     # (90,220), # text space
     #     (100,100), # text space GOOD for 1000x1000
@@ -60,11 +60,13 @@ def TextToIcon(Text, TemplateImage, Font, FilePath, CourseSeed):
     cv2.imwrite(FilePath, result_o)
 
 def GetNamesFromShnaton(CourseNumber: int, Year: int):
-    ShnatonJson = requests.get("https://shnaton.huji.ac.il/api/courses/code/"+CourseNumber+"?year="+str(Year))
+    # get the json from the shanton:
+    # TODO: fake headers to look like a browser (only needed if blocked.)
+    ShnatonJson = requests.get("https://shnaton.huji.ac.il/api/courses/code/"+CourseNumber+"?year="+str(Year)) 
 
-    if ShnatonJson.status_code == 200:
+    if ShnatonJson.status_code == 200: # dont trip over the network TODO: make this an assert.
         ShantonObject = json.loads(ShnatonJson.content)
-        if ShantonObject[0]['code'] == CourseNumber:
+        if ShantonObject[0]['code'] == CourseNumber: # make sure we got the right course. TODO: make this an assert.
             print(CourseNumber + "good")
         else:
             print(CourseNumber + "bad")
@@ -73,34 +75,39 @@ def GetNamesFromShnaton(CourseNumber: int, Year: int):
     else:
         exit()
 
-def FUCKTheAcademyoftheHebrewLanguage(NameOfClass):
+def StringCleaner(NameOfClass):
+    # clean up the string
+    # TODO: count the amount of spaces as not to insert too many line retuns 
+
     # StringToBeRevesed = ((NameOfClass[0]['name']['he']).replace(' ', '\n')).replace('(',"}").replace(')',"{")
     StringToBeRevesed = (((NameOfClass).replace(' ', '\n')).replace('(',"").replace(')',"")).replace("-","")
         
     return(StringToBeRevesed)
 
-def AddTextToImage(Text, OutputFile, Year, CouseNumber):
+def AddTextToImageAndDealWithString(Text, OutputFile, Year, CouseNumber):
     StringForImage = ""
     for line in Text.splitlines():
         print(line[::-1])
+
         # StringForImage += (line[::-1])+"\n"
-        # StringForImage += (line[::-1])+"\n"
-        StringForImage += get_display(line)+"\n"
+        StringForImage += get_display(line)+"\n" # revese the string in a way that does not change any latin text
         
     TextToIcon(StringForImage+Year, "Template.png", "fonts/Alef-Bold.ttf", OutputFile, CouseNumber)
 
-
 def GetCourseFromFile():
+    # read the file from disk.
     with open("source",'r') as SourceList:
         SourceData = SourceList.read().splitlines()
     return(SourceData)
+
+
 
 CourseList = GetCourseFromFile()
 
 for Course in CourseList:
     print(Course)
-    CourseName = GetNamesFromShnaton(Course, 2027)
+    CourseName = GetNamesFromShnaton(Course, 2027) # year is used for the api
     # TextForImage = get_display(CourseName)
-    TextForImage = FUCKTheAcademyoftheHebrewLanguage(CourseName)
+    TextForImage = StringCleaner(CourseName)
 
-    AddTextToImage(TextForImage, Course+".png", "2026-2027", Course)
+    AddTextToImageAndDealWithString(TextForImage, Course+".png", "2026-2027", Course) # the year here is any text to be added to the bottom of the logo.
