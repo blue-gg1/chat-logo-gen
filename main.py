a4d06d1f851346f726167ed4e1bc2f7fbc0feafa
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-09-01 12:44:58 +0300

    you fucked me

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def ClankerGetTestDateFromShnaton(ShnatonId: int, Year: int):
 
         if name in ("Written Exam", "Written test", "Mid-term Exams", "First partial test", "Second partial test", "Exam"):
             for schedule in assignment.get("schedules", []):
-                TestDates.append((schedule["startTime"]).strftime("%d.%m.%Y"))
+                TestDates.append((schedule["startTime"]))
                 
     # pprint.pp(assignments)
 
